4659fb5154520a99c2aedeaf5cbba57b22e9ee40
lruizap <lruizap2502@gmail.com>
2024-07-29 18:14:42 +0200

    Deploy changes

--- school_empower/dist/_next/static/chunks/app/layout-7f6a2290b9bf4b70.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{238:function(a,n,e){Promise.resolve().then(e.t.bind(e,5910,23))},5910:function(a){a.exports={style:{fontFamily:"'__Montserrat_b1da2a', '__Montserrat_Fallback_b1da2a'",fontStyle:"normal"},className:"__className_b1da2a"}}},function(a){a.O(0,[934,971,23,744],function(){return a(a.s=238)}),_N_E=a.O()}]);
